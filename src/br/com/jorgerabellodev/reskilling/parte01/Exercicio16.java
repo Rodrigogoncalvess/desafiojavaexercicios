8163e8738af962df406d592a4448dee2d0408a72
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-24 19:25:01 -0300

    Exercícios 16

--- src/br/com/jorgerabellodev/reskilling/parte01/Exercicio16.java
+++ src/br/com/jorgerabellodev/reskilling/parte01/Exercicio16.java
@@ -1,6 +1,10 @@
 package br.com.jorgerabellodev.reskilling.parte01;
 
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import static java.lang.String.format;
 
 /**
  * Faça um programa que receba o valor dos catetos de um triângulo, calcule e mostre o valor da hipo-tenusa
@@ -8,14 +12,18 @@ import java.util.Scanner;
 public class Exercicio16 {
     public static void main(String[] args) {
 
-        int catetoOposto;
-        int pontaB;
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Informe o valor do cateto oposto");
-        catetoOposto=scanner.
-
-
-
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Informe o valor do cateto oposto:");
+            double catetoOposto = scanner.nextInt();
+            System.out.println("Informe o valor do cateto adjacente:");
+            double catetoAdjacente = scanner.nextInt();
+            double valor = ((Math.pow(catetoAdjacente, 2)) + (Math.pow(catetoOposto, 2)));
+            System.out.println("Valor da hipotenusa é: " +(decimalFormat.format( Math.sqrt(valor))));
+        }catch (InputMismatchException i){
+            System.out.println("O programa não aceita letra ou simbolo.");
+        }
     }
 }
