302817f1c1b9f41ecc38be002be748dbd5493148
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-28 09:14:03 -0300

    Exercício 24

--- src/br/com/jorgerabellodev/reskilling/parte02/Exercicio24.java
+++ src/br/com/jorgerabellodev/reskilling/parte02/Exercicio24.java
@@ -1,5 +1,8 @@
 package br.com.jorgerabellodev.reskilling.parte02;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Faça um programa que receba o preço, a categoria (1 — limpeza; 2 — alimentação; ou 3 — vestuário)
  * e a situação (R — produtos que necessitam de refrigeração; e N — produtos que não necessitam de re-
@@ -10,12 +13,12 @@ package br.com.jorgerabellodev.reskilling.parte02;
  * ------------------------------------------------------------
  * PREÇO           CATEGORIA           PERCENTUAL DE AUMENTO
  * ------------------------------------------------------------
- * 1                       5%
- * <= 25               2                       8%
- * 3                       10%
+ *                         1                       5%
+ * <= 25                   2                       8%
+ *                         3                       10%
  * ------------------------------------------------------------
  * 1                       12%
- * > 25                2                       15%
+ * > 25                     2                       15%
  * 3                       18%
  * ------------------------------------------------------------
  * <p>
@@ -35,4 +38,139 @@ package br.com.jorgerabellodev.reskilling.parte02;
  * ------------------------------------------------------------
  */
 public class Exercicio24 {
+    public static void main(String[] args) {
+
+        var preco = 0.0;
+        var categoria = 0;
+        var situacao = "";
+        var aumento = "Valor do aumento:%.2f ";
+        var precoNovo = 0.0;
+        var porcentagem = 0.0;
+        var valorDigitado = "Valor digitado: ";
+        var valorNovo = "Valor novo: ";
+        var refrigeracao = "Produtos que necessitam de refrigeração";
+        var semRefrigeracao = "Produtos que não necessitam de refrigeração";
+
+        var scanner = new Scanner(System.in);
+
+
+        try {
+            System.out.println("Informe o preço:");
+            preco = scanner.nextDouble();
+
+            System.out.println("Informe a categoria:" + "\n 1- Limpeza" + "\n 2 -Alimentação" + "\n 3 - Vestuário");
+            categoria = scanner.nextInt();
+        } catch (InputMismatchException i) {
+            System.out.println("Programa não pode inserir letra ou símbolo.");
+        }
+        while (!situacao.equals("R") && !situacao.equals("N")) {
+            System.out.println("Informe a situação: \n R - Produtos que necessitam de refrigeração \n N - Produtos que não necessitam de refrigeração ");
+            situacao = scanner.next();
+
+            if (!situacao.equals("R") && !situacao.equals("N")) {
+                System.out.println("Digite uma das opções listado abaixo:\n");
+            }
+        }
+        switch (categoria) {
+            case 1:
+                if (preco <= 25) {
+                    porcentagem = preco * 0.05;
+                    precoNovo = porcentagem + preco;
+                    System.out.printf(aumento , porcentagem);
+
+                } else if (preco != 25) {
+                    porcentagem = preco * 0.12;
+                    precoNovo = porcentagem + preco;
+                    System.out.printf(aumento , porcentagem);
+                }
+                if (precoNovo <= 50) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Barato");
+
+                }
+                if (precoNovo >= 51 && precoNovo <= 120) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Normal");
+
+                }
+                if (precoNovo >= 121) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Caro");
+                }
+                if (situacao.equals("R")) {
+                    System.out.println(refrigeracao);
+                }
+                if (situacao.equals("N")) {
+                    System.out.println(semRefrigeracao);
+                }
+                break;
+            case 2:
+                if (preco <= 25) {
+                    porcentagem = preco * 0.08;
+                    precoNovo = porcentagem + preco;
+                    System.out.printf(aumento , porcentagem);
+                } else if (preco > 25) {
+                    porcentagem = preco * 0.15;
+                    precoNovo = porcentagem + preco;
+                    System.out.printf(aumento , porcentagem);
+                }
+                if (precoNovo <= 50) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Barato");
+
+                }
+                if (precoNovo >= 51 && precoNovo <= 120) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Normal");
+
+                }
+                if (precoNovo >= 121) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Caro");
+                }
+                if (situacao.equals("R")) {
+                    System.out.println(refrigeracao);
+                }
+                if (situacao.equals("N")) {
+                    System.out.println(semRefrigeracao);
+                }
+                break;
+
+            case 3:
+                if (preco <= 25) {
+                    porcentagem = preco * 0.10;
+                    precoNovo = porcentagem + preco;
+                    System.out.printf(aumento , porcentagem);
+                } else if (preco > 18) {
+                    porcentagem = preco * 0.15;
+                    precoNovo = porcentagem + preco;
+                    System.out.printf(aumento , porcentagem);
+                }
+                if (precoNovo <= 50) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Barato");
+
+                }
+                if (precoNovo >= 51 && precoNovo <= 120) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Normal");
+
+                }
+                if (precoNovo >= 121) {
+                    System.out.println(valorDigitado + preco);
+                    System.out.println(valorNovo + precoNovo + " Classificado como Caro");
+                }
+                if (situacao.equals("R")) {
+                    System.out.println(refrigeracao);
+                }
+                if (situacao.equals("N")) {
+                    System.out.println(semRefrigeracao);
+                }
+                break;
+
+            default:
+        }
+
+
+    }
 }
