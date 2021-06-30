99d3ad1c36d429bb8165bef919f3da2c692b1c5a
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-30 08:15:55 -0300

    Exercício 18

--- src/br/com/jorgerabellodev/reskilling/parte03/Exercicio18.java
+++ src/br/com/jorgerabellodev/reskilling/parte03/Exercicio18.java
@@ -1,5 +1,9 @@
 package br.com.jorgerabellodev.reskilling.parte03;
 
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Foi feita uma pesquisa entre os habitantes de uma região. Foram coletados os dados de idade, sexo (M/F)
  * e salário. Faça um programa que calcule e mostre:
@@ -10,4 +14,59 @@ package br.com.jorgerabellodev.reskilling.parte03;
  * Finalize a entrada de dados ao ser digitada uma idade negativa.
  */
 public class Exercicio18 {
+    public static void main(String[] args) {
+
+        var idade = new int[11];
+        var sexo = new String[11];
+        var salario = new double[11];
+        var mediaSalario = 0.0;
+        var qtdFeminino = 0.0;
+        var menorIdade = Integer.MAX_VALUE;
+        var maiorIdade = 0;
+        var decimal = new DecimalFormat("0.00");
+
+        var teclado = new Scanner(System.in);
+        try {
+            for (int i = 1; i < idade.length; i++) {
+                System.out.println("Informe sua idade: ");
+                idade[i] = teclado.nextInt();
+                if (idade[i] == 0) {
+                    System.out.println("Programa finalizado!");
+                    System.exit(0);
+                }
+
+                System.out.println("Informe seu sexo: \nM - Masculino \nF - Feminino");
+                sexo[i] = teclado.next();
+                while (!sexo[i].equals("M") && !sexo[i].equals("F")) {
+                    System.out.println("Informe seu sexo: \nM - Masculino \nF - Feminino ");
+                    sexo[i] = teclado.next();
+                }
+                System.out.println("Informe seu salário");
+                salario[i] = teclado.nextDouble();
+            }
+            for (int i = 1; i < idade.length; i++) {
+                mediaSalario += salario[i] / salario.length;
+
+                if (idade[i] == 0) {
+                    break;
+                }
+                if (idade[i] > maiorIdade) {
+                    maiorIdade = idade[i];
+                }
+                if (idade[i] < menorIdade) {
+                    menorIdade = idade[i];
+                }
+                if (salario[i] <= 200) {
+                    System.out.println("Idade " + idade[i] + " da " + i + "° pessoa e seu sexo " + sexo[i]);
+                }
+            }
+            System.out.println("Média dos salários do grupo: " + (decimal.format(mediaSalario)));
+            System.out.println("Quantidade de mulheres com salário até R$ 200,00 = " + qtdFeminino);
+            System.out.println("Menor idade do grupo: " + menorIdade);
+            System.out.println("Maior idade do grupo: " + maiorIdade);
+
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Programa não pode inserir letra ou símbolo.");
+        }
+    }
 }
