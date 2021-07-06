5a468b44825b6118355534cf44f9d20edcc43cbe
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-06 09:00:06 -0300

    Exercício 12

--- src/br/com/jorgerabellodev/reskilling/parte04/Exercicio12.java
+++ src/br/com/jorgerabellodev/reskilling/parte04/Exercicio12.java
@@ -1,5 +1,7 @@
 package br.com.jorgerabellodev.reskilling.parte04;
 
+import java.util.Scanner;
+
 /**
  * Faça um programa que receba cinco números e mostre a saída a seguir:
  * Digite o 1o número 5
@@ -10,4 +12,20 @@ package br.com.jorgerabellodev.reskilling.parte04;
  * Os números digitados foram: 5 + 3 + 2 + 0 + 2 = 12
  */
 public class Exercicio12 {
+    public static void main(String[] args) {
+
+        var numero = new int[6];
+        var somaNumeros = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 1; i < numero.length; i++) {
+            System.out.println("Digite o " + i + "° número: ");
+            numero[i] = scanner.nextInt();
+            somaNumeros += numero[i];
+
+        }  System.out.println("Os números digitados foram: " + numero[1] + " + "+ numero[2]  + " + " + numero[3]  + " + " + numero[4] + " + " +numero[5]  + " = " + somaNumeros);
+
+    }
 }
