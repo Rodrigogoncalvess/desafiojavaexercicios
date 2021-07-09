c986ed3affc89d48d993d5d8629ffe9c9a3dbb35
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-09 09:55:44 -0300

    Exercício 12

--- src/br/com/jorgerabellodev/reskilling/parte05/Exercicio12.java
+++ src/br/com/jorgerabellodev/reskilling/parte05/Exercicio12.java
@@ -10,4 +10,26 @@ package br.com.jorgerabellodev.reskilling.parte05;
  * ■ ■ a média de idade dos alunos que não fizeram nenhuma prova em alguma disciplina. Cuidado para não contar duas vezes o mesmo aluno.
  */
 public class Exercicio12 {
+    public static void main(String[] args) {
+
+
+        int[] vetor = {0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 10, 11, 12, 13, 14, 15};
+        int cont = 0;
+        int matriz[][] = new int[4][4];
+
+        for (int col = 0; col < matriz.length; col++) {
+            for (int lin = 0; lin < matriz.length; lin++) {
+                matriz[lin][col] = vetor[cont];
+                cont++;
+            }
+        }
+
+        //Exibindo Valores
+        for (int col = 0; col < matriz.length; col++) {
+            for (int lin = 0; lin < matriz.length; lin++) {
+                System.out.print(matriz[lin][col] + "\t");
+            }
+            System.out.println("");
+        }
+    }
 }
