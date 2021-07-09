4f31af74d9e2a739358a9c95d55e8892236c9f66
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-09 12:53:52 -0300

    Exercício 12

--- src/br/com/jorgerabellodev/reskilling/parte05/Exercicio12.java
+++ src/br/com/jorgerabellodev/reskilling/parte05/Exercicio12.java
@@ -1,35 +1,78 @@
 package br.com.jorgerabellodev.reskilling.parte05;
 
+import java.util.Scanner;
+
 /**
  * O programa deverá calcular e mostrar:
  * ■ ■ a quantidade de alunos com idade entre 18 e 25 anos que fizeram mais de duas provas em determinada
  * disciplina cujo código é digitado pelo usuário. O usuário poderá digitar um código não cadastrado; nesse caso,
  * o programa deverá mostrar uma mensagem de erro;
+ * <p>
  * ■ ■ uma listagem contendo o código dos alunos que fizeram menos que três provas em determinada disciplina,
  * seguido do código da disciplina;
+ * <p>
  * ■ ■ a média de idade dos alunos que não fizeram nenhuma prova em alguma disciplina. Cuidado para não contar duas vezes o mesmo aluno.
  */
 public class Exercicio12 {
     public static void main(String[] args) {
 
+        int[] idadeAluno = new int[]{19, 18, 24, 25};
+        int[] codigoAluno = new int[]{11, 22, 33, 44};
+        int[] codigoDisciplina = new int[]{123, 234, 345, 345};
+        int qtdAluno = 0;
+        int[] qtdProva = new int[]{5, 0, 3, 5};
+        int mediaIdadeAluno = 0;
+        int qtdProvaNaoFeita = 0;
+        int totalIdadeAluno = 0;
+        int consultarDisciplina;
+        boolean existeDisciplina = false;
 
-        int[] vetor = {0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 10, 11, 12, 13, 14, 15};
-        int cont = 0;
-        int matriz[][] = new int[4][4];
+        Scanner scanner = new Scanner((System.in));
 
-        for (int col = 0; col < matriz.length; col++) {
-            for (int lin = 0; lin < matriz.length; lin++) {
-                matriz[lin][col] = vetor[cont];
-                cont++;
+
+        System.out.println("Digite o código da disciplina que deseja obter informações: ");
+        consultarDisciplina = scanner.nextInt();
+
+        for (int i = 0; i < codigoDisciplina.length; i++) {
+            if (codigoDisciplina[i] == consultarDisciplina) {
+                existeDisciplina = true;
+
             }
         }
 
-        //Exibindo Valores
-        for (int col = 0; col < matriz.length; col++) {
-            for (int lin = 0; lin < matriz.length; lin++) {
-                System.out.print(matriz[lin][col] + "\t");
+        while (existeDisciplina == false) {
+
+            System.out.println("Código inválido, digite novamente.");
+            consultarDisciplina = scanner.nextInt();
+
+            for (int i = 0; i < codigoDisciplina.length; i++) {
+                if (codigoDisciplina[i] == consultarDisciplina) {
+                    existeDisciplina = true;
+                }
             }
-            System.out.println("");
         }
+        for (int i = 0; i < codigoDisciplina.length; i++) {
+            if ((consultarDisciplina == codigoDisciplina[i]) && (idadeAluno[i] >= 18 && idadeAluno[i] <= 25) && (qtdProva[i] >= 2)) {
+                qtdAluno++;
+            }
+        }
+        System.out.println("Disciplina consultada: " + consultarDisciplina);
+        System.out.println("Quantidade de alunos com idade entre 18 e 25 anos que fizeram mais de duas provas: " + qtdAluno + "\n");
+
+
+        for (int i = 0; i < idadeAluno.length; i++) {
+            if (qtdProva[i] < 3) {
+                System.out.println("Código dos alunos que fizeram menos que 3 provas: " + codigoAluno[i]);
+                System.out.println("Código da disciplina: " + codigoDisciplina[i] + "\n");
+            }
+            if (qtdProva[i] == 0) {
+                qtdProvaNaoFeita++;
+                totalIdadeAluno += idadeAluno[i];
+                mediaIdadeAluno = totalIdadeAluno / qtdProvaNaoFeita;
+            }
+        }
+        System.out.println("Media das idades dos alunos que não fizeram prova: " + mediaIdadeAluno);
+        System.out.println("Quantidade de alunos que não fizeram prova: " + qtdProvaNaoFeita);
     }
 }
+
