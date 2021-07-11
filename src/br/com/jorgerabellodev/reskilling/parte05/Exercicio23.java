88c99748ed7469ddf9568d4ffcc2b9601f3b1332
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-11 14:19:09 -0300

    Exercício 23

--- src/br/com/jorgerabellodev/reskilling/parte05/Exercicio23.java
+++ src/br/com/jorgerabellodev/reskilling/parte05/Exercicio23.java
@@ -1,5 +1,7 @@
 package br.com.jorgerabellodev.reskilling.parte05;
 
+import java.util.Random;
+
 /**
  * Faça um programa que preencha uma matriz 3 x 4, calcule e mostre:
  * ■ ■ a quantidade de elementos pares;
@@ -7,4 +9,42 @@ package br.com.jorgerabellodev.reskilling.parte05;
  * ■ ■ a média de todos os elementos.
  */
 public class Exercicio23 {
+    public static void main(String[] args) {
+
+
+        final int coluna = 3;
+        final var linha = 4;
+
+        var matriz = new int[coluna][linha];
+        var qtdPares = 0;
+        var somaImpares = 0;
+        var somaTodos = 0.0;
+        var mediaElemento = 0.0;
+        var qtdElemento = 0;
+
+        Random random = new Random();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = random.nextInt(100);
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] % 2 == 0) {
+                    qtdPares++;
+                }
+                if (matriz[i][j] % 2 == 1) {
+                    somaImpares += matriz[i][j];
+                }
+                somaTodos += matriz[i][j];
+                qtdElemento++;
+            }
+            mediaElemento = somaTodos / qtdElemento;
+        }
+        System.out.println("Quantidade de elementos pares: " + qtdPares);
+        System.out.println("Soma dos elementos ímpares: " + somaImpares);
+        System.out.println("Média de todos os elementos: " + mediaElemento);
+
+    }
 }
