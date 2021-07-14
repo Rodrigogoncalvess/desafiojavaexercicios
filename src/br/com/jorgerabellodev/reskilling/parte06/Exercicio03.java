620bfb2ca0dcf8e488cbb8b1a40d6cd8b0d7697d
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-14 10:25:54 -0300

    Exercício 03

--- src/br/com/jorgerabellodev/reskilling/parte06/Exercicio03.java
+++ src/br/com/jorgerabellodev/reskilling/parte06/Exercicio03.java
@@ -1,8 +1,66 @@
 package br.com.jorgerabellodev.reskilling.parte06;
 
+import java.util.Scanner;
+
 /**
  * Elabore uma sub-rotina que receba dois números como parâmetros e retorne 0, se o primeiro número for
  * divisível pelo segundo número. Caso contrário, deverá retornar o próximo divisor.
  */
 public class Exercicio03 {
+
+
+    static int numero1;
+    static int numero2;
+
+    public static void main(String[] args) {
+
+        SubRotina subRotina = new SubRotina();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Primeiro número:");
+        numero1 = scanner.nextInt();
+        while (numero1 == 0) {
+            if (numero1 == 0) {
+                System.out.println("Primeiro número não pode ser zero, digite um valor válido:");
+                numero1 = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Segundo número:");
+        numero2 = scanner.nextInt();
+
+        while (numero1 < numero2) {
+            if (numero1 < numero2) {
+
+                System.out.println("Digite novamente o segundo numero:");
+                numero2 = scanner.nextInt();
+            }
+        }
+
+        subRotina.divisor();
+
+    }
+
+    static class SubRotina {
+        public void divisor() {
+            if (numero1 % numero2 == 0) {
+                System.out.println("Primeiro número é divisível pelo segundo!");
+                return;
+            }
+            for (int i = numero2; i <= numero1; i++) {
+                if (numero1 % i == 0) {
+                    System.out.println("Próximo divisor é " + i);
+                    break;
+                }
+            }
+
+        }
+    }
+
 }
+
+
+
+
+
