8ff5ef107d30cfe6e9d383585f124f92bcb7475b
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-14 10:26:48 -0300

    Exercício 08

--- src/br/com/jorgerabellodev/reskilling/parte06/Exercicio08.java
+++ src/br/com/jorgerabellodev/reskilling/parte06/Exercicio08.java
@@ -1,7 +1,38 @@
 package br.com.jorgerabellodev.reskilling.parte06;
 
+import java.util.Scanner;
+
 /**
  * Faça uma sub-rotina que receba um valor inteiro e positivo, calcule e mostre seu fatorial.
  */
 public class Exercicio08 {
+
+    static int total = 1;
+
+    public static void main(String[] args) {
+
+        int numero;
+
+        SubRotina subRotina = new SubRotina();
+
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Digite um número: ");
+        numero = scan.nextInt();
+
+        System.out.println("Valor do fatorial é " + subRotina.multiplicacao(numero));
+
+
+    }
+
+    static class SubRotina {
+        public int multiplicacao(int valor) {
+            for (int i = 2; i <= valor; i++) {
+                total *= i;
+
+            }
+            return total;
+        }
+    }
 }
