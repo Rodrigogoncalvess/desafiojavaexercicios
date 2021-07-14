11ffcc6c6af8b8a146fd69bb8509b0c31fd533d0
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-14 10:26:05 -0300

    Exercício 04

--- src/br/com/jorgerabellodev/reskilling/parte06/Exercicio04.java
+++ src/br/com/jorgerabellodev/reskilling/parte06/Exercicio04.java
@@ -1,8 +1,39 @@
 package br.com.jorgerabellodev.reskilling.parte06;
 
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Faça uma sub-rotina que receba como parâmetro o raio de uma esfera, calcule e mostre no programa principal
  * o seu volume: v = 4/3 * R^3.
  */
 public class Exercicio04 {
+
+    public static void main(String[] args) {
+
+        double esfera;
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.000");
+
+        Scanner scanner = new Scanner(System.in);
+
+        SubRotina sub = new SubRotina();
+
+        try {
+            System.out.println("Informe o raio da esfera para calcular seu volume: ");
+            esfera = scanner.nextDouble();
+
+            System.out.println("O seu volume é: " + decimalFormat.format(sub.raio(esfera)));
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Programa não pode inserir letra ou símbolo.");
+        }
+    }
+
+    static class SubRotina {
+        public double raio(double valor) {
+            valor = (3 * Math.pow(valor, 3) / 4);
+            return valor;
+        }
+    }
 }
