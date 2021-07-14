8b32ce4ac228b5c5b846bc29942a274e461fd079
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-14 10:28:16 -0300

    Exercício 13

--- src/br/com/jorgerabellodev/reskilling/parte06/Exercicio13.java
+++ src/br/com/jorgerabellodev/reskilling/parte06/Exercicio13.java
@@ -1,5 +1,7 @@
 package br.com.jorgerabellodev.reskilling.parte06;
 
+import java.util.Scanner;
+
 /**
  * Foi realizada uma pesquisa entre 15 habitantes de uma região. Os dados coletados de cada habitante
  * foram: idade, sexo, salário e número de filhos.
@@ -9,4 +11,86 @@ package br.com.jorgerabellodev.reskilling.parte06;
  * uma sub-rotina para cada cálculo).
  */
 public class Exercicio13 {
+
+    static int[] idade = new int[15];
+    static double[] salario = new double[idade.length];
+    static String[] sexo = new String[idade.length];
+    static int[] numeroFilho = new int[idade.length];
+
+
+    public static void main(String[] args) {
+
+
+        SubRotina subRotina = new SubRotina();
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < idade.length; i++) {
+
+            System.out.println("Informe sua idade: ");
+            idade[i] = scanner.nextInt();
+
+            System.out.println("Informe seu sexo: ");
+            sexo[i] = scanner.next();
+
+            while (!sexo[i].equalsIgnoreCase("masculino") && !sexo[i].equalsIgnoreCase("feminino")) {
+                System.out.println("Informe seu sexo: ");
+                sexo[i] = scanner.next();
+            }
+
+            System.out.println("Informe seu salário: ");
+            salario[i] = scanner.nextDouble();
+
+            System.out.println("Informe a quantidade de filhos: ");
+            numeroFilho[i] = scanner.nextInt();
+
+
+        }
+        subRotina.calculoMediaSalario(salario);
+        subRotina.calculoIdade(idade);
+        subRotina.calculoFilho(numeroFilho);
+
+    }
+
+    static class SubRotina {
+
+        public void calculoMediaSalario(double[] vetorSalario) {
+            double somaSalario = 0.0;
+            double mediaSalario = 0.0;
+
+            for (int i = 0; i < vetorSalario.length; i++) {
+                somaSalario += vetorSalario[i];
+                mediaSalario = somaSalario / vetorSalario.length;
+            }
+            System.out.println("Média de salário entre os habitantes: " + mediaSalario);
+
+        }
+
+        public void calculoIdade(int[] vetorIdade) {
+            int idadeMaior = 0;
+            int idadeMenor = Integer.MAX_VALUE;
+
+            for (int i = 0; i < vetorIdade.length; i++) {
+                if (vetorIdade[i] > idadeMaior) {
+                    idadeMaior = vetorIdade[i];
+                }
+                if (vetorIdade[i] < idadeMenor) {
+                    idadeMenor = vetorIdade[i];
+                }
+            }
+            System.out.println("Maior idade do grupo: " + idadeMaior);
+            System.out.println("Menor idade do grupo: " + idadeMenor);
+
+        }
+
+        public void calculoFilho(int[] vetorFilho) {
+            int qtdMulheres = 0;
+            for (int i = 0; i < vetorFilho.length; i++) {
+                if (!sexo.equals("feminino") && (numeroFilho[i] == 3) && (salario[i] <= 500))
+                    qtdMulheres++;
+            }
+            System.out.println("Quantidade de mulheres com três filhos que recebem até R$ 500,00: " + qtdMulheres);
+        }
+    }
 }
+
