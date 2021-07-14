1fe84cca3248b5b38c0664aefbf8d2d840e4f91b
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-14 10:27:13 -0300

    Exercício 09

--- src/br/com/jorgerabellodev/reskilling/parte06/Exercicio09.java
+++ src/br/com/jorgerabellodev/reskilling/parte06/Exercicio09.java
@@ -1,8 +1,39 @@
 package br.com.jorgerabellodev.reskilling.parte06;
 
+import java.util.Scanner;
+
 /**
  * Crie uma sub-rotina que receba como parâmetro um valor inteiro e positivo e retorne a soma dos divisores
  * desse valor.
  */
 public class Exercicio09 {
+
+    static int total;
+
+    public static void main(String[] args) {
+
+        int numero;
+
+        SubRotina subRotina = new SubRotina();
+
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Digite um número: ");
+        numero = scan.nextInt();
+
+        System.out.println("Soma dos divisores desse número é " + subRotina.divisores(numero));
+
+    }
+
+    static class SubRotina {
+        public int divisores(int valor) {
+            for (int i = 1; i < valor; i++) {
+                if (valor % i == 0) {
+                    total += i;
+                }
+            }
+            return total;
+        }
+    }
 }
