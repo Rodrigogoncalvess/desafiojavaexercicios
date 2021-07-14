1366a49d1a3b5c43e06c1d78bcf746292ff958d0
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-14 10:26:14 -0300

    Exercício 05

--- src/br/com/jorgerabellodev/reskilling/parte06/Exercicio05.java
+++ src/br/com/jorgerabellodev/reskilling/parte06/Exercicio05.java
@@ -1,7 +1,41 @@
 package br.com.jorgerabellodev.reskilling.parte06;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Faça uma sub-rotina que receba um valor inteiro e verifique se ele é positivo ou negativo.
  */
 public class Exercicio05 {
+
+    public static void main(String[] args) {
+
+        int numero;
+
+        Scanner teclado = new Scanner(System.in);
+
+        SubRotina sub = new SubRotina();
+
+        try {
+            System.out.println("Digite um valor: ");
+            numero = teclado.nextInt();
+
+            System.out.println(sub.validador(numero));
+
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Programa não pode inserir letra ou símbolo.");
+        }
+    }
+
+    static class SubRotina {
+        public int validador(int numero) {
+
+            if (numero >= 0) {
+                System.out.println("Número digitado é positivo!");
+            } else {
+                System.out.println("Número digitado é negativo!");
+            }
+            return numero;
+        }
+    }
 }
