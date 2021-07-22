b8a993aecbd48a520fe4efec4ba0bff14250f8e7
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-22 12:41:23 -0300

    Exercício 25

--- src/br/com/jorgerabellodev/reskilling/parte07/Exercicio25.java
+++ src/br/com/jorgerabellodev/reskilling/parte07/Exercicio25.java
@@ -5,4 +5,16 @@ package br.com.jorgerabellodev.reskilling.parte07;
  * programa deverá armazenar essas vogais e suas posições originais em vetores, mostrar a frase criptografada e posteriormente descriptografá-la.
  */
 public class Exercicio25 {
+    public static void main(String[] args) {
+
+        String frase = "programa vogais";
+
+        String novaFrase;
+
+        novaFrase = frase.replaceAll("[aeiouAEIOU]", "");
+
+        System.out.println("Frase criptografada: " + novaFrase);
+        System.out.println("Frase descriptografada: " + frase);
+
+    }
 }
