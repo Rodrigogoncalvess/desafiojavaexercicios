5a7592183ddace4a344bd61be3718b2856b78a26
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-22 12:39:49 -0300

    Exercício 17

--- src/br/com/jorgerabellodev/reskilling/parte07/Exercicio17.java
+++ src/br/com/jorgerabellodev/reskilling/parte07/Exercicio17.java
@@ -12,4 +12,57 @@ package br.com.jorgerabellodev.reskilling.parte07;
  * Saída: Cmaosca jsaandeilraosloc
  */
 public class Exercicio17 {
+
+    public static void main(String[] args) {
+        String frase = "Aula com bola";
+        System.out.println(criptografia(frase));
+    }
+
+    static String criptografia(String frase) {
+
+        String[] palavras = frase.split(" ");
+
+        boolean ePar = palavras.length % 2 == 0;
+
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            for (int i = 0; i < palavras.length; i += 2) {
+                builder.append(joinRevert(new String[]{palavras[i], palavras[i + 1]})).append(" ");
+            }
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+
+        }
+        if (!ePar) {
+            builder.append(reverse(palavras[palavras.length - 1]));
+        }
+        return builder.toString();
+    }
+
+
+    static String reverse(String palavra) {
+        return new StringBuilder(palavra).reverse().toString();
+    }
+
+    static String joinRevert(String[] palavras) {
+        String segunda = reverse(palavras[1]);
+        StringBuilder join = new StringBuilder();
+        int i = 0;
+        while (true) {
+            if (i < palavras[0].length()) {
+                join.append(palavras[0].charAt(i));
+            }
+            if (i < segunda.length()) {
+                join.append(segunda.charAt(i));
+            }
+            if (i > segunda.length() && i > palavras[0].length()) {
+                break;
+            }
+            i++;
+        }
+        return join.toString();
+    }
 }
+
+
+
