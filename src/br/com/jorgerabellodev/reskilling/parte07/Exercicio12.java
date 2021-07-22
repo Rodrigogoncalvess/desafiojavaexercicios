7746497e63993573c06a10ec0e3c485d9d4202f1
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-22 12:38:43 -0300

    Exercício 12

--- src/br/com/jorgerabellodev/reskilling/parte07/Exercicio12.java
+++ src/br/com/jorgerabellodev/reskilling/parte07/Exercicio12.java
@@ -1,7 +1,22 @@
 package br.com.jorgerabellodev.reskilling.parte07;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Faça um programa que receba uma data no formato DD/MM/AAAA e a mostre com o mês por extenso: DD/mês por extenso/AAAA
  */
 public class Exercicio12 {
+    public static void main(String[] args) {
+
+        Date dataHoje = new Date();
+
+        DateFormat dateFormat = new SimpleDateFormat("dd/MMMM/yyyy");
+        String dataFormatada = dateFormat.format(dataHoje);
+
+        System.out.println("Data formatada: " + dataFormatada);
+
+
+    }
 }
