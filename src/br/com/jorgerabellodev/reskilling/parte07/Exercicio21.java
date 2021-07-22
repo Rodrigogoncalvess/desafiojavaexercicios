0f9f54bab54da9d463e0f36e37a17f978bf02c60
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-22 12:40:51 -0300

    Exercício 21

--- src/br/com/jorgerabellodev/reskilling/parte07/Exercicio21.java
+++ src/br/com/jorgerabellodev/reskilling/parte07/Exercicio21.java
@@ -8,4 +8,19 @@ package br.com.jorgerabellodev.reskilling.parte07;
  * Saída: CÉU E LUA ESTÃO NO ESTRELAS
  */
 public class Exercicio21 {
+    public static void main(String[] args) {
+
+        String frase = "ESTRELAS E LUA ESTÃO NO CÉU";
+
+        String[] novaFrase = frase.split(" ");
+
+        String primeriraPosiao = novaFrase[0];
+        String posicaoFinal = novaFrase[novaFrase.length - 1];
+
+        novaFrase[novaFrase.length - 1] = primeriraPosiao;
+        novaFrase[0] = posicaoFinal;
+
+        System.out.println(String.join(" ", novaFrase));
+
+    }
 }
