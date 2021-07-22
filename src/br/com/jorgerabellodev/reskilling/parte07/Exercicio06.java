023bac9850860c627a32f3d5fc9aa644a03e00e6
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-22 12:37:39 -0300

    Exercício 06

--- src/br/com/jorgerabellodev/reskilling/parte07/Exercicio06.java
+++ src/br/com/jorgerabellodev/reskilling/parte07/Exercicio06.java
@@ -7,4 +7,17 @@ package br.com.jorgerabellodev.reskilling.parte07;
  * Saída: PPRROOGGRRAAMMAARR ÉÉ BBOOMM
  */
 public class Exercicio06 {
+    public static void main(String[] args) {
+
+        String frase = "PROGRAMAR É BOM";
+
+        StringBuilder novaFrase = new StringBuilder();
+
+        for (char caracter : frase.toCharArray()) {
+            novaFrase.append(caracter).append(caracter);
+        }
+
+        System.out.println(novaFrase);
+
+    }
 }
