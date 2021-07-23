13dbc463f7ce532dc677a02b05ba3e31f7ceb156
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-07-23 16:28:58 -0300

    Exercício 22

--- src/br/com/jorgerabellodev/reskilling/parte02/Exercicio22.java
+++ src/br/com/jorgerabellodev/reskilling/parte02/Exercicio22.java
@@ -1,5 +1,8 @@
 package br.com.jorgerabellodev.reskilling.parte02;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Faça um programa que receba a idade e o peso de uma pessoa. De acordo com a tabela a seguir, verifique
  * e mostre em qual grupo de risco essa pessoa se encaixa.
@@ -14,4 +17,52 @@ package br.com.jorgerabellodev.reskilling.parte02;
  * --------------------------------------------------------------------------------------------------------------------
  */
 public class Exercicio22 {
+    public static void main(String[] args) {
+
+        int idade;
+        double peso;
+
+        Scanner teclado = new Scanner(System.in);
+
+        try {
+            System.out.println("Informe sua idade e seu peso para saber qual é seu grupo de risco:");
+            System.out.println("\nDigite sua idade:");
+            idade = teclado.nextInt();
+
+            System.out.println("Digite seu peso:");
+            peso = teclado.nextDouble();
+
+
+            if (idade < 20 && peso <= 60) {
+                System.out.println("Grupo de risco é 9");
+
+            } else if (idade < 20 && peso > 60 && peso <= 90) {
+                System.out.println("Grupo de risco é 8");
+
+            } else if ((idade < 20) && (peso > 90)) {
+                System.out.println("Grupo de risco é 7");
+
+            } else if ((idade >= 20) && (idade <= 50) && (peso <= 60)) {
+                System.out.println("Grupo de risco é 6");
+
+            } else if ((idade >= 20) && (idade <= 50) && (peso > 60) && (peso <= 90)) {
+                System.out.println("Grupo de risco é 5");
+
+            } else if ((idade >= 20) && (idade <= 50) && (peso > 90)) {
+                System.out.println("Grupo de risco é 4");
+
+            } else if (idade > 50 && peso <= 60) {
+                System.out.println("Grupo de risco é 3");
+
+            } else if ((idade > 50) && (peso > 60) && (peso <= 90)) {
+                System.out.println("Grupo de risco é 2");
+
+            } else if (idade > 50 && peso > 90) {
+                System.out.println("Grupo de risco é 1");
+
+            }
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Programa não pode ser digitado letra ou simbolo!");
+        }
+    }
 }
