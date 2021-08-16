3d0531ee241b23140acfba8538802702f1bffc3f
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:09:01 -0300

    Exercício 22

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio22.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio22.java
@@ -1,7 +1,19 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa para consultar os produtos com preços inferiores a R$ 15,00.
  */
 public class Exercicio22 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoProdutos arquivoProdutos = new ArquivoProdutos();
+
+        arquivoProdutos.todasProdutosTempararios();
+
+        arquivoProdutos.consultarProdutoPrecoMenorQuinze();
+
+    }
+
 }
