4fa1cc1131d8d5388fe4119d505c69b9a90dd0e6
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:05:52 -0300

    Exercício 06

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio06.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio06.java
@@ -1,8 +1,17 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa que altere um recebimento de um cliente, ou seja, o usuário informa o número do docu-
  * mento e o número do cliente e faz as alterações desejadas.
  */
 public class Exercicio06 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoRecebimento arquivoRecebimento = new ArquivoRecebimento();
+
+        arquivoRecebimento.alterarRecebimentoDoCliente();
+
+    }
 }
