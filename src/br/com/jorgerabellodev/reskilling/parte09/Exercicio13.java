873b07ba375d347691d7971cb6ab92fa2cd13d67
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:07:10 -0300

    Exercício 13

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio13.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio13.java
@@ -1,5 +1,7 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa para:
  * ■ ■ Cadastrar as estações climáticas, por exemplo, primavera-verão e outono-inverno.
@@ -8,4 +10,23 @@ package br.com.jorgerabellodev.reskilling.parte09;
  * ■ ■ Mostrar um relatório de todas as roupas de determinada estação, informando, inclusive, o nome do estilista que as desenhou.
  */
 public class Exercicio13 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoEstacao arquivoEstacao = new ArquivoEstacao();
+        ArquivoEstilista arquivoEstilista = new ArquivoEstilista();
+        ArquivoRoupa arquivoRoupa = new ArquivoRoupa();
+
+        arquivoEstacao.cadastrarEstacao();
+
+        arquivoEstilista.cadastrarEstilista();
+
+        arquivoRoupa.cadastrarRoupa();
+
+        arquivoRoupa.todasRoupasTempararios();
+
+        arquivoEstilista.todosEstilistasTempararios();
+
+        arquivoRoupa.todasRoupasEstacao();
+    }
+
 }
