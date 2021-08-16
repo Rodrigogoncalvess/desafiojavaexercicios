ace547f72b52a688db4ac07825b4dc7d084017ef
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:05:41 -0300

    Exercício 05

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio05.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio05.java
@@ -1,8 +1,19 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa que altere o cadastro de clientes do Exercício 1. O usuário deve informar o código do
  * cliente que será alterado.
  */
 public class Exercicio05 {
+
+    public static void main(String[] args) throws IOException {
+
+        ArquivoCliente arquivoCliente = new ArquivoCliente();
+
+        arquivoCliente.alterarCadastroCliente();
+
+    }
+
 }
