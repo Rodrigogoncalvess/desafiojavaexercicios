9bf0222cc66dddccd821d89adefa34ab147f706d
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:05:32 -0300

    Exercício 04

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio04.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio04.java
@@ -1,8 +1,18 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa que exclua clientes e, consequentemente, todos os seus recebimentos, dos arquivos criados
  * no Exercício 1.
  */
 public class Exercicio04 {
+
+    public static void main(String[] args) throws IOException {
+
+        ArquivoCliente arquivoCliente = new ArquivoCliente();
+
+        arquivoCliente.excluirCliente();
+
+    }
 }
