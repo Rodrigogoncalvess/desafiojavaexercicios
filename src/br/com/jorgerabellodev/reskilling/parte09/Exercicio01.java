20b561b9867a292b6260de62fd364647903d7b20
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:04:56 -0300

    Exercício 01

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio01.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio01.java
@@ -1,5 +1,8 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Faça um programa para criar os arquivos a seguir:
  * ------------------------------------------------------------
@@ -13,4 +16,53 @@ package br.com.jorgerabellodev.reskilling.parte09;
  * ------------------------------------------------------------
  */
 public class Exercicio01 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        ArquivoCliente arquivoCliente = new ArquivoCliente();
+
+        int opcao;
+
+
+        Scanner ler = new Scanner(System.in);
+
+
+        System.out.println("1 - Criar arquivo CLIENTES");
+        System.out.println("2 - Criar arquivo RECEBIMENTOS");
+        opcao = ler.nextInt();
+
+        switch (opcao) {
+            case 1:
+                arquivoCliente.criarArquivo("CLIENTES", opcao);
+                break;
+
+            case 2:
+                arquivoCliente.criarArquivo("RECEBIMENTOS", opcao);
+                break;
+
+            default:
+                System.out.println("Error..");
+                break;
+        }
+        while (opcao != 2) {
+            System.out.println("\n\nDeseja continuar 1 - SIM / 2 - NÃO");
+            opcao = ler.nextInt();
+            if (opcao == 1) {
+
+                break;
+            } else if (opcao == 2) {
+                System.out.println("Obrigado por usar o programa!");
+
+                return;
+            } else {
+                System.out.println("Opção inválida");
+            }
+        }
+
+    }
+
+
 }
+
+
