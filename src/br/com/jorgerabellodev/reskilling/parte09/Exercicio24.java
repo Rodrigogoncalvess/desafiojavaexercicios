fdaa5dc72c4e7d52f332b674bb265971df3a8960
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:09:20 -0300

    Exercício 24

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio24.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio24.java
@@ -1,7 +1,17 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa para excluir todos os produtos com preço superior a R$ 200,00.
  */
 public class Exercicio24 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoProdutos arquivoProdutos = new ArquivoProdutos();
+
+        arquivoProdutos.excluirProdutosComPrecoSuperiorDuzentos();
+
+
+    }
 }
