a6b114c0cbde701c677226759ce7ea98184304e2
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:08:02 -0300

    Exercício 18

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio18.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio18.java
@@ -1,7 +1,17 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa para aumentar os preços de todos os produtos em 15%.
  */
 public class Exercicio18 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoProdutos arquivoProdutos = new ArquivoProdutos();
+
+        arquivoProdutos.todasProdutosTempararios();
+
+        arquivoProdutos.aumentarPrecoEmQuinzePorcento();
+    }
 }
