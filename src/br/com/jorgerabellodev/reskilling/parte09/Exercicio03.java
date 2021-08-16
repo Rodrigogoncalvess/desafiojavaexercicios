bb844451d1b854fd58dc2d68286408c4411c8759
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:05:23 -0300

    Exercício 03

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio03.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio03.java
@@ -1,8 +1,20 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+
+import java.io.IOException;
+
 /**
  * Faça um programa que inclua recebimentos no arquivo criado no Exercício 1, devendo verificar se o cliente
  * já se encontra cadastrado.
  */
 public class Exercicio03 {
+    public static void main(String[] args) throws IOException {
+
+
+        ArquivoRecebimento arquivoRecebimento = new ArquivoRecebimento();
+
+        arquivoRecebimento.cadastrarRecebimentos();
+    }
 }
+
+
