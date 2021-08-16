3c69dd1a2abba5d26ff6502848136b0c143431a5
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:07:49 -0300

    Exercício 17

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio17.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio17.java
@@ -1,7 +1,17 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa para consultar a descrição de todos os produtos que possuem preço superior a R$ 500,00.
  */
 public class Exercicio17 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoProdutos arquivoProdutos = new ArquivoProdutos();
+
+        arquivoProdutos.todasProdutosTempararios();
+
+        arquivoProdutos.buscarProdutoMaiorQueQuinhetos();
+    }
 }
