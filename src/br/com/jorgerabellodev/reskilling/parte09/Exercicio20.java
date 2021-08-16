5335c0a33e8348ae29e7914dc17b4503c76359ec
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:08:39 -0300

    Exercício 20

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio20.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio20.java
@@ -1,7 +1,17 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa para consultar todos os produtos cujos nomes comecem pela letra M.
  */
 public class Exercicio20 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoProdutos arquivoProdutos = new ArquivoProdutos();
+
+        arquivoProdutos.todasProdutosTempararios();
+
+        arquivoProdutos.buscarProdutoPorLetra();
+    }
 }
