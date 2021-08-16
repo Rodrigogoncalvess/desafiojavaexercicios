f1f975b813dac643a4bbebe2c734a45e1a281a5e
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:08:53 -0300

    Exercício 21

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio21.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio21.java
@@ -1,7 +1,16 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa para excluir produtos do arquivo criado no Exercício 15.
  */
 public class Exercicio21 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoProdutos arquivoProdutos = new ArquivoProdutos();
+
+        arquivoProdutos.excluirProduto();
+
+    }
 }
