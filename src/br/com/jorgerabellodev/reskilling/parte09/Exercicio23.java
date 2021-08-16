b802cb702286649de2f9e2a931d26fb4a95ca2aa
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-08-16 18:09:11 -0300

    Exercício 23

--- src/br/com/jorgerabellodev/reskilling/parte09/Exercicio23.java
+++ src/br/com/jorgerabellodev/reskilling/parte09/Exercicio23.java
@@ -1,8 +1,22 @@
 package br.com.jorgerabellodev.reskilling.parte09;
 
+import java.io.IOException;
+
 /**
  * Faça um programa para consultar todos os produtos com nomes iniciados por uma letra digitada pelo
  * usuário e com preços entre dois valores também fornecidos pelo usuário.
  */
 public class Exercicio23 {
+    public static void main(String[] args) throws IOException {
+
+        ArquivoProdutos arquivoProdutos = new ArquivoProdutos();
+
+        arquivoProdutos.todasProdutosTempararios();
+
+        arquivoProdutos.buscarProdutoPorLetraDigitada();
+
+
+    }
+
+
 }
